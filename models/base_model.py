90f4ad3f5dfc0427253f79a5dfe69b1520583920
shehab910 <shehabihab@hotmail.com>
2024-05-03 19:36:51 +0300

    Refactor BaseModel to re-create an instance with a dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python3
-# Write a class BaseModel that defines all common attributes/methods for other classes:
-
-# models/base_model.py
-# Public instance attributes:
-# id: string - assign with an uuid when an instance is created:
-# you can use uuid.uuid4() to generate unique id but don’t forget to convert to a string
-# the goal is to have unique id for each BaseModel
-# created_at: datetime - assign with the current datetime when an instance is created
-# updated_at: datetime - assign with the current datetime when an instance is created and it will be updated every time you change your object
-# __str__: should print: [<class name>] (<self.id>) <self.__dict__>
-# Public instance methods:
-# save(self): updates the public instance attribute updated_at with the current datetime
-# to_dict(self): returns a dictionary containing all keys/values of __dict__ of the instance:
-# by using self.__dict__, only instance attributes set will be returned
-# a key __class__ must be added to this dictionary with the class name of the object
-# created_at and updated_at must be converted to string object in ISO format:
-# format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
-# you can use isoformat() of datetime object
-# This method will be the first piece of the serialization/deserialization process: create a dictionary representation with “simple object type” of our BaseModel
-
+"""BaseModel module"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel():
     """BaseModel class"""
-
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """BaseModel constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            return
+        for k, v in kwargs.items():
+            if k == "created_at" or k == "updated_at":
+                v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+            if k != "__class__":
+                setattr(self, k, v)
 
     def __str__(self):
         """Return string representation of BaseModel instance"""
