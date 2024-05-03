4445724a3ff939faeb354e8f373846ce8a62ca48
shehab910 <shehabihab@hotmail.com>
2024-05-03 19:29:36 +0300

    chore: Update test_base_model.py to include additional test cases for BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
 	def test_init(self):
+		""" """
 		b = BaseModel()
 		self.assertIsInstance(b, BaseModel)
 		self.assertIsInstance(b.id, str)
@@ -11,14 +12,18 @@ class TestBaseModel(unittest.TestCase):
 		self.assertIsInstance(b.updated_at, datetime)
 
 	def test_str(self):
+		""" """
 		b = BaseModel()
 		self.assertIsInstance(b.__str__(), str)
 
 	def test_save(self):
+		""" """
 		b = BaseModel()
 		b.save()
 		self.assertIsInstance(b.updated_at, datetime)
+		self.assertNotEqual(b.created_at, b.updated_at)
 
 	def test_to_dict(self):
+		""" """
 		b = BaseModel()
 		self.assertIsInstance(b.to_dict(), dict)
