edd958ce560c3839b302f6c3badfb703ed8a1099
shehab910 <shehabihab@hotmail.com>
2024-05-03 19:14:00 +0300

    Add AUTHORS file and BaseModel class implementation

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,24 @@
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+class TestBaseModel(unittest.TestCase):
+	def test_init(self):
+		b = BaseModel()
+		self.assertIsInstance(b, BaseModel)
+		self.assertIsInstance(b.id, str)
+		self.assertIsInstance(b.created_at, datetime)
+		self.assertIsInstance(b.updated_at, datetime)
+
+	def test_str(self):
+		b = BaseModel()
+		self.assertIsInstance(b.__str__(), str)
+
+	def test_save(self):
+		b = BaseModel()
+		b.save()
+		self.assertIsInstance(b.updated_at, datetime)
+
+	def test_to_dict(self):
+		b = BaseModel()
+		self.assertIsInstance(b.to_dict(), dict)
