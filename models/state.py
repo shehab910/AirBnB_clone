d0d94089297182cd45d2a3a96c852753dca28e22
shehab910 <shehabihab@hotmail.com>
2024-05-04 14:21:51 +0300

    ./models/state.py

--- models/state.py
+++ models/state.py
@@ -4,10 +4,10 @@ from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """State class"""
-    name = ""
+  """State class"""
+  name = ""
 
-    def __init__(self, *args, **kwargs):
-        """State constructor"""
-        super().__init__(*args, **kwargs)
-        # self.name = kwargs.get("name", "")
+  def __init__(self, *args, **kwargs):
+    """State constructor"""
+    super().__init__(*args, **kwargs)
+    self.name = kwargs.get("name", "")
